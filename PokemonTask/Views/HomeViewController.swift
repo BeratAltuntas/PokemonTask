7ed1c3246727049d91ac079ea48e73b2a81ec9d4
Berat Altuntaş <cberataltuntas@gmail.com>
2022-07-29 17:47:49 +0300

    UI is half done

--- PokemonTask/Views/HomeViewController.swift
+++ PokemonTask/Views/HomeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private var scrollView: UIScrollView!
+    private var contentView: UIView!
     private var refreshButton: CustomUIButton!
     private var cardView: CustomUIView!
     
@@ -16,9 +18,37 @@ class HomeViewController: UIViewController {
         setupView()
     }
     
+    private func setupScrollView() {
+        scrollView = UIScrollView()
+        contentView = UIView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+        NSLayoutConstraint.activate([
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.heightAnchor.constraint(equalToConstant: 890),
+            
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 890)
+        ])
+        
+        contentView.backgroundColor = ViewConstants.customBackgroundColor
+        scrollView.backgroundColor = ViewConstants.customBackgroundColor
+    }
+    
     private func setupView() {
-        view.backgroundColor = ViewConstants.customBackgroundColor
+        setupScrollView()
         createRefreshButton()
+        createPokemonViewContainer()
     }
     
     private func createRefreshButton() {
@@ -38,24 +68,35 @@ class HomeViewController: UIViewController {
         refreshButton.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(font: .rounded(ofSize: 24, weight: .black)), forImageIn: .normal)
         
         refreshButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(refreshButton)
+        contentView.addSubview(refreshButton)
+        
         
         NSLayoutConstraint.activate([
-            refreshButton.topAnchor.constraint(equalTo: view.topAnchor, constant: ViewConstants.customRefreshButtonTopConstant.ToCGFloat()),
-            refreshButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ViewConstants.customRefreshButtonLeftConstant.ToCGFloat()),
+            refreshButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: ViewConstants.customRefreshButtonTopConstant.ToCGFloat()),
+            refreshButton.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: ViewConstants.customRefreshButtonLeftConstant.ToCGFloat()),
             refreshButton.widthAnchor.constraint(equalToConstant: ViewConstants.customRefreshButtonWidth.ToCGFloat()),
             refreshButton.heightAnchor.constraint(equalToConstant: ViewConstants.customRefreshButtonHeight.ToCGFloat())
         ])
     }
     
-    private func createView() {
+    private func createPokemonViewContainer() {
         cardView = CustomUIView()
         cardView.translatesAutoresizingMaskIntoConstraints = false
         cardView.cornerRadius = ViewConstants.customUIViewCornerRadius.ToCGFloat()
-        view.addSubview(cardView)
+        cardView.backgroundColor = .white
+        
+        // CardView Shadow
+        cardView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.40).cgColor
+        cardView.layer.shadowOffset = CGSize(width: 1.0, height: 3.0)
+        cardView.layer.shadowOpacity = 2.5
+        cardView.layer.shadowRadius = 3.5
+        contentView.addSubview(cardView)
         
         NSLayoutConstraint.activate([
-            
+            cardView.topAnchor.constraint(lessThanOrEqualTo: refreshButton.bottomAnchor, constant: 86),
+            cardView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 45),
+            cardView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -45),
+            cardView.bottomAnchor.constraint(greaterThanOrEqualTo: contentView.bottomAnchor, constant: -182),
         ])
         
     }
